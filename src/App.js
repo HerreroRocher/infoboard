ff4d5d0b03b8a17b8573baec61a8a8368f000df1
Daniel Herrero <daniel.herrero@student.manchester.ac.uk>
2024-07-28 01:50:23 +0100

    Made template

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from './logo.svg'; // Import the logo and styles
+import './App.css'; // Import the CSS for styling
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Description /> {/* Description component for the header */}
+      <Weather /> {/* Weather component to display weather info */}
+      <div className="bustimebox-container"> {/* Container for the bus time boxes */}
+        <BusTimeBox float="left" /> {/* First bus time box */}
+        <BusTimeBox float="center" /> {/* Second bus time box */}
+        <BusTimeBox float="right" /> {/* Third bus time box */}
+      </div>
     </div>
   );
+}
+
+function Description() {
+  return (
+    <header className="App-header">
+      <p>
+        Daniel's infoboard using ReactJS {/* The main header text */}
+      </p>
+    </header>
+  )
+}
+
+function Weather() {
+  return (
+    <div className="weather">
+      <p>
+        This is where the weather will go {/* Placeholder for weather info */}
+      </p>
+    </div>
+  )
+}
+
+function BusTimeBox({ float }) {
+  return (
+    <div className="bustimebox" style={{ float: { float } }}>
+      <p>
+        This is where a specific bus time will go {/* Placeholder for bus times */}
+      </p>
+    </div>
+  )
 }
 
 export default App;
